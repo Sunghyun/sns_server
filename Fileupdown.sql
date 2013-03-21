721babb59de4388df7643fc431607a9a054af930
Sunghyun <zerocool0713@naver.com>
2013-03-21 16:17:54 +0900

    re?re?

--- Fileupdown.sql
+++ Fileupdown.sql
@@ -3,12 +3,12 @@ num number primary key,
 id varchar(20),
 fid varchar(500),
 loc varchar(500)
-)
+);
 
 create sequence file_seq
 increment by 1
 start with 1
-maxvalue 9999
+maxvalue 9999;
 
 delete table files;
 
